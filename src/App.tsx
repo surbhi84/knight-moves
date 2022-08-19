cb7b269b151e8a14063d6ecb37587ccf2aaba59e
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2022-08-19 16:22:49 +0530

    add: footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ChessboardComponent } from "./components/ChessboardComponent";
 
 const App = () => {
   return (
-    <div className="">
+    <div className="flex flex-col h-screen ">
       <header className=" flex gap-1 items-center text-2xl sm:text-3xl font-semibold p-2 px-10">
         <img src="/knight.svg" alt="logo" className="h-6 sm:h-10" />
 
@@ -10,13 +10,16 @@ const App = () => {
           <span className="  text-teal-500">Knight</span> Moves
         </h1>
       </header>
-      <main className="flex flex-col items-center">
+      <main className="flex flex-col items-center p-10">
         <p className="text-xl pt-10 pb-5">
           Hey, select any position and we'll highlight all the possible moves
           for you 🤙
         </p>
         <ChessboardComponent />
       </main>
+      <footer className="flex items-center justify-center text-lg mt-auto gap-1 bg-teal-200 p-4">
+        Made by Surbhi Kukreti 💫
+      </footer>
     </div>
   );
 };
